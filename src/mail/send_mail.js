b9b676a352a932b334412999bc386c6378ac494b
fakhrulzulfi <fakhrulzulfi@gmail.com>
2022-03-06 18:36:07 +0700

    Add configuration mail

---
+++ src/mail/send_mail.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PWD
+    }
+});
+
+const sendEmail = data => {
+    transporter.sendMail(data, (err, info) => {
+        if( err ) throw err;
+        console.log('Email sent: ' + info.response);
+    });
+};
+
+module.exports = { sendEmail };
