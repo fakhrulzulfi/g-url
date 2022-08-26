bbb182807cd52ab9d57a08b4a58df00d7e31c779
Fakhrul Zulfi <fakhrulzulfi@gmail.com>
2022-08-26 13:44:45 +0700

    repair sending email features with nodemailer and gmail oauth2

--- src/mail/send_mail.js
+++ src/mail/send_mail.js
@@ -1,26 +1,45 @@
 require('dotenv').config();
 
 const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
+const OAuth2 = google.auth.OAuth2;
 
-const auth = {
-    type: process.env.AUTH_TYPE,
-    user: process.env.AUTH_USER,
-    clientId: process.env.AUTH_CLIENTID,
-    clientSecret: process.env.AUTH_CLIENTSECRET,
-    refreshToken: process.env.AUTH_REFRESHTOKEN,
-    accessToken: process.env.AUTH_ACCESSTOKEN
+const createTransporter = async () => {
+    const oauth2Client = new OAuth2(
+        process.env.AUTH_CLIENTID,
+        process.env.AUTH_CLIENTSECRET,
+        "https://developers.google.com/oauthplayground"
+    );
+    
+    oauth2Client.setCredentials({
+        refresh_token: process.env.AUTH_REFRESHTOKEN
+    });
+
+    const accessTokenOAuth2 = await new Promise((resolve, reject) => {
+        oauth2Client.getAccessToken((err, token) => {
+            if( err ) reject();
+            resolve(token);
+        });
+    });
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            type: 'OAuth2',
+            user: process.env.AUTH_USER,
+            clientId: process.env.AUTH_CLIENTID,
+            clientSecret: process.env.AUTH_CLIENTSECRET,
+            refreshToken: process.env.AUTH_REFRESHTOKEN,
+            accessToken: accessTokenOAuth2
+        }
+    });
+
+    return transporter;
 };
 
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth
-});
-
-const sendEmail = data => {
-    transporter.sendMail(data, (err, info) => {
-        if( err ) throw err;
-        console.log('Email sent: ' + info.response);
-    });
+const sendEmail = async (data) => {
+    const emailTransporter = await createTransporter();
+    await emailTransporter.sendMail(data);
 };
 
 module.exports = { sendEmail };
